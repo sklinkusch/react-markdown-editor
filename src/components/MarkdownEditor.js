920754873537143f3b2d14ca13d6da601708f29d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-04 12:00:03 +0200

    updated state

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-let Remarkable = require("remarkable");
-let md = new Remarkable();
+// import Remarkable from "remarkable";
+// let md = new Remarkable();
 
 export default class MarkdownEditor extends Component {
   constructor(props) {
@@ -9,12 +9,20 @@ export default class MarkdownEditor extends Component {
       value: "Hello, **World**!"
     };
   }
+  handleChange = e => {
+    this.setState({ value: e });
+  };
   render() {
     return (
       <div className="MarkdownEditor">
         <h2>Input</h2>
         <h3>Enter some markdown</h3>
-        <textarea rows="4" cols="60" value={this.state.value} />
+        <textarea
+          rows="4"
+          cols="60"
+          defaultValue={this.state.value}
+          onChange={e => this.handleChange(e.target.value)}
+        />
         <h2>Output</h2>
         <p className="output" />
       </div>
