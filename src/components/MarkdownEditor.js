ed115e67c82a228ae4ab7e7d2051789cab414479
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-04 11:35:59 +0200

    initialized value of textarea as a state

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+let Remarkable = require("remarkable");
+let md = new Remarkable();
 
 export default class MarkdownEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Hello, **World**!"
+    };
+  }
   render() {
     return (
       <div className="MarkdownEditor">
         <h2>Input</h2>
         <h3>Enter some markdown</h3>
-        <textarea rows="4" cols="60" />
+        <textarea rows="4" cols="60" value={this.state.value} />
         <h2>Output</h2>
         <p className="output" />
       </div>
